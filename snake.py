d2bbf5d01e0abe453c4889d07b5e26fc2bda31cf
Christian Hernandez <herc991207@gs.utm.mx>
2022-12-06 23:13:45 -0600

    Primera subida de gato

---
+++ snake.py
@@ -0,0 +1,92 @@
+import random
+from os import system
+
+def run():
+    IMAGES = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+    BD = ["DIEGO","CHRISTIAN","EDUARDO","JOHN","BRANDON"]
+
+    word = random.choice(BD)
+    spaces = ["_"]*len(word)
+    attemps = 6
+
+    while True:
+        system("cls")
+        for character in spaces:
+                print(character,end=" ")
+        print(IMAGES[attemps])
+        letter = input("Elige una letra:").upper()
+
+
+        found = False
+        for idx, character in enumerate(word):
+            if character == letter:
+                spaces[idx] = letter
+                found = True
+        if not found:
+            attemps -= 1
+
+        if "_" not in spaces:
+            system("cls")
+            print("GANASTE")
+            break
+            input()
+
+        if attemps == 0:
+            system("cls")
+            print("PERDISTE")
+            break
+            input()
+        
+if __name__ == '__main__':
+    run()
+    
