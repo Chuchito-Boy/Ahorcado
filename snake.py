e533d509b269dcfba75eb92fd7c6b2228f8785c1
luisdiego4k <gsantibamoran@gmail.com>
2022-12-07 01:02:31 -0600

    Modificacion de interfaz y diccionario

--- snake.py
+++ snake.py
@@ -1,93 +1,102 @@
 import random
 from os import system
 
+
 def run():
     IMAGES = ['''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''']
+    🧱🧱🧱🧱🧱🧱🧱🧱
+    🧱     ❗     🧱
+    🧱            🧱
+    🧱            🧱
+    🧱            🧱
+    🧱            🧱
+    🧱            🧱
+    🧱🧱🧱🧱🧱🧱🧱🧱''', '''
+    🧱🧱🧱🧱🧱🧱🧱🧱
+    🧱     ❗     🧱
+    🧱     😀     🧱
+    🧱            🧱
+    🧱            🧱
+    🧱            🧱
+    🧱            🧱
+    🧱🧱🧱🧱🧱🧱🧱🧱''', '''
+    🧱🧱🧱🧱🧱🧱🧱🧱
+    🧱     ❗     🧱
+    🧱     😀     🧱
+    🧱     ❗     🧱
+    🧱     ❗     🧱
+    🧱            🧱
+    🧱            🧱
+    🧱🧱🧱🧱🧱🧱🧱🧱''', '''
+    🧱🧱🧱🧱🧱🧱🧱🧱
+    🧱     ❗     🧱
+    🧱     🤨     🧱
+    🧱   💪❗     🧱
+    🧱     ❗     🧱
+    🧱            🧱
+    🧱            🧱
+    🧱🧱🧱🧱🧱🧱🧱🧱''', '''
+    🧱🧱🧱🧱🧱🧱🧱🧱
+    🧱     ❗     🧱
+    🧱     🤨     🧱
+    🧱   💪❗👍   🧱
+    🧱     ❗     🧱
+    🧱            🧱
+    🧱            🧱
+    🧱🧱🧱🧱🧱🧱🧱🧱''', '''
+    🧱🧱🧱🧱🧱🧱🧱🧱
+    🧱     ❗     🧱
+    🧱     🤨     🧱
+    🧱   💪❗👍   🧱
+    🧱     ❗     🧱
+    🧱   🦶       🧱
+    🧱            🧱
+    🧱🧱🧱🧱🧱🧱🧱🧱''', '''
+    🧱🧱🧱🧱🧱🧱🧱🧱
+    🧱     ❗     🧱
+    🧱     💀     🧱
+    🧱   💪❗👍   🧱
+    🧱     ❗     🧱
+    🧱   🦶  🦵   🧱
+    🧱            🧱
+    🧱🧱🧱🧱🧱🧱🧱🧱''']
 
-                    BD = ["DIEGO","CHRISTIAN","EDUARDO","JOHN","BRANDON"]
+    BD = ["CONEJO","RINOCERONTE","JIRAFA","LEOPARDO","CEBRA","BUFALO","HORMIGA","COCODRILO","ELEFANTE","DELFIN","PATO","PERRO","GATO","CANGREJO","PULPO","TORTUGA","BALLENA","FOCA","OSO","ZORRO","KOALA","MONO","POLLO","VACA"]
 
     word = random.choice(BD)
-    spaces = ["_"]*len(word)
+    print()
+    spaces = ["➖"]*len(word)
     attemps = 0
 
     while True:
-        system("cls")
-        for character in spaces:
-                print(character,end=" ")
-        print(IMAGES[attemps])
-        letter = input("Elige una letra:").upper()
-
-
-        found = False
-        for idx, character in enumerate(word):
-            if character == letter:
-                spaces[idx] = letter
-                found = True
-
-        if not found:     
-            if attemps <= 6:
-                attemps += 1
-
-        if "_" not in spaces:
             system("cls")
-            print("GANASTE")
-            break
-            input()
+            for character in spaces:
+                    print(character,end=" ")
+            print(IMAGES[attemps])
+            letter = input("Elige una letra: ").upper()
 
-        if attemps > 6:
-            system("cls")
-            print("PERDISTE")
-            break
-            input()
+
+            found = False
+            for idx, character in enumerate(word):
+                if character == letter:
+                    spaces[idx] = letter
+                    found = True
+
+            if not found:
+                if attemps <= 6:
+                    attemps += 1
+
+            if "➖" not in spaces:
+                system("cls")
+                print("GANASTE")
+                break
+                input()
+
+            if attemps > 6:
+                system("cls")
+                print("PERDISTE")
+                break
+                input()
 
 if __name__ == '__main__':
     run()
